b2be5e78e5bd2734613459580155df33247c6dd9
blackchestnut <alexander.kalinichev@gmail.com>
2019-08-12 16:41:11 +0300

    misc

--- resources/rails_backup.rb
+++ resources/rails_backup.rb
@@ -42,7 +42,7 @@ action :create do
     CONFIG
   end
 
-  if backup_directories&.any?
+  if backup_directories && backup_directories.any?
     directories = backup_directories.map do |v|
       "          directory.add \"#{app.dir :root}/#{v}\""
     end.join("\n")
