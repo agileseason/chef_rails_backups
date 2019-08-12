7384ff7cd0890f3b8c225c4301614a2f0fe2327f
blackchestnut <alexander.kalinichev@gmail.com>
2019-08-12 16:36:51 +0300

    Update version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 name             'chef_rails_backups'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures chef_rails_backups'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'backup'
 depends 'git'
